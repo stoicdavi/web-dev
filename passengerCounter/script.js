22fc5efec3b728d50b71ccc0e4efb63a9c6e4bd0
David Nanjila <nanjiladavid2@gmail.com>
2023-09-21 02:03:47 +0300

    added a functonality that displays the current number when save button is clicked

--- passengerCounter/script.js
+++ passengerCounter/script.js
@@ -2,11 +2,15 @@ let count = 0;
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
+let countN = document.getElementById("coun-number")
+let saveEl = document.getElementById("save-El")
 function increment()
 {
     count += 1;
-    document.getElementById("number").innerText = count;
+    countN.innerText = count
 }
 function save(){
+    let countStr = count + "  -  "
     console.log(count)
+    saveEl.textContent += countStr
 }
