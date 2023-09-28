847c4a42e1e01be97c3922169a876a37fb2b9ea3
David Nanjila <nanjiladavid2@gmail.com>
2023-09-28 22:40:05 +0300

    my fully working simple counter app

--- passengerCounter/script.js
+++ passengerCounter/script.js
@@ -4,13 +4,22 @@ let count = 0;
 // change the count-el in the HTML to reflect the new count
 let countN = document.getElementById("coun-number")
 let saveEl = document.getElementById("save-El")
+let Cls = document.getElementById("clear1")
 function increment()
 {
     count += 1;
     countN.innerText = count
 }
+
 function save(){
     let countStr = count + "  -  "
     console.log(count)
     saveEl.textContent += countStr
+    countN.innerText = 0
+    count = 0
+}
+function clear(){
+        count = 0;
+        countN.innerText = 0;
+        saveEl.textContent = "";
 }
