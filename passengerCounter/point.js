55f6aafbef34b11b4eb7ed7851c0af04c4f58a5d
David Nanjila <nanjiladavid2@gmail.com>
2023-09-29 08:25:23 +0300

    This is a simple Html javaScript code that displays on the console the result of addition and subtraction using javascript functions

---
+++ passengerCounter/point.js
@@ -0,0 +1,18 @@
+let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+function add3(){
+    myPoints += 3
+}
+add3()
+add3()
+add3()
+function remove1(){
+    myPoints -= 1
+}
+remove1()
+remove1()
+
+// Call the functions to that the line below logs out 10
+console.log(myPoints)
