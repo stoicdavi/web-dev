60f442dce82e9345749263a50f9f1f058a1e2e50
David Nanjila <nanjiladavid2@gmail.com>
2023-09-29 08:38:23 +0300

    a simple funtion that displays error message when a user clicks the buy button on the website

---
+++ passengerCounter/renderMs.js
@@ -0,0 +1,6 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+function display(){
+    document.getElementById('error').textContent = "Error Please try again later"
+}
