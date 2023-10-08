265ba8fe8297be288002e7e82f9c293b876dda88
David Nanjila <nanjiladavid2@gmail.com>
2023-10-08 13:36:02 +0300

    added a funtion to generate a whole number positive random numbers

--- javaScriptPracs/simpleCalc/blackjack.js
+++ javaScriptPracs/simpleCalc/blackjack.js
@@ -4,18 +4,24 @@
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
-let firstCard = 6
-let secondCard = 9
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let card = [firstCard, secondCard]
 let hasBlacjack = false
 let isAlive = true
+let sum = firstCard + secondCard 
 let thirdNumber = 6
 let message = ''
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementsByClassName("sum")
+let sumEl = document.getElementById("sum")
 let cardsEl = document.getElementById("cards")
 
-let sum = firstCard + secondCard 
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() * 12) + 1
+    return randomNumber
+}
+
+
 function play(){
     renderGame()
 }
@@ -29,20 +35,20 @@ function renderGame()
         cardsEl.textContent += card[i] + " "
     }
     document.getElementById('dplay').textContent = "Do you want to draw another card? 😊"
-    document.querySelector("#sum").textContent = "Your Score: "+ sum//using query selector instead of getElementById
+    sumEl.textContent = "Your Score: "+ sum//using query selector instead of getElementById
     isAlive = true
    
     } 
     else if(sum === 21){
     document.getElementById('dplay').innerText = 'hellooooo, you got a black jack✨🎉🎉'
-    document.getElementById("sum").textContent = "Your Score: "+ sum
+    sumEl.textContent = "Your Score: "+ sum
     hasBlacjack = true
     isAlive = false
     cardsEl.textContent= "Cards: " + firstCard +" "+ secondCard
     }
     else{
             document.getElementById('dplay').textContent = "You are out of the game😢"
-            document.getElementById("sum").textContent = "Your Score: "+ sum
+            sumEl.textContent = "Your Score: "+ sum
             isAlive = false
     }
     //checking whether the uplayer has won a blackjack and reward him/her
@@ -53,9 +59,9 @@ function renderGame()
 }
 
 function drawaNew(){
-    play();
-    let card3= 8
-    card.push(card3)
+    let card3 = getRandomCard()
+    card.push(card3) 
+    sum += card3
     renderGame()
     
 }
